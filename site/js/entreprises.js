8510e2e7bb620d92b58fa3428a5ebd7020e522e6
Alexandre <algaotr67@hotmail.fr>
2021-07-05 14:28:23 +0200

    Nouvexu ajouts
    Ajout d'animations et de quelques scripts sur la page entreprises

---
+++ site/js/entreprises.js
@@ -0,0 +1,20 @@
+
+/* On rège ici le border-radius des blocks de calendrier */
+let event_top = $('.upper-block .event-block');
+let event_bottom = $('.lower-block .event-block');
+
+if($(window).width() > 750) {
+    event_bottom.first().addClass("event-bottom-left");
+} else {
+    event_top.last().addClass("event-bottom-left");
+}
+
+$(window).on('resize',function(){
+    if($(window).width() > 750) {
+        event_bottom.first().addClass("event-bottom-left");
+        event_top.last().removeClass("event-bottom-left");
+    } else {
+        event_top.last().addClass("event-bottom-left");
+        event_bottom.first().removeClass("event-bottom-left");
+    }
+})
