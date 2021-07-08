8826a53f3913b83317ff94157ba1415e8c68bd1c
Dredd14 <leochaillard@gmail.com>
2021-07-08 12:04:29 +0200

    Calendrier: popup lorsqu'on click sur les jours

--- site/js/entreprises.js
+++ site/js/entreprises.js
@@ -8,4 +8,23 @@ lines.first().addClass("event-bottom-left");
 lines.last().addClass("event-bottom-right");
 
 window.addEventListener('load', AOS.refresh);
+
+/* On gère les intéractions avec le calendrier */
+let cases = $('.event-block > .day');
+let overlay = $('.day-page-background');
+let close = $('.close-day-page');
+
+
+cases.on('click',function(){
+  overlay.css('display','block');
+  $('html').css('overflow','hidden');
+});
+
+
+close.on('click',function(){
+  overlay.css('display','none');
+  $('html').css('overflow','scroll');
+});
+
+
 })
