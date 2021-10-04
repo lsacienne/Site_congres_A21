421324aeeb1d33b35d122be73694e8196f93254f
Alexandre <algaotr67@hotmail.fr>
2021-10-04 15:55:43 +0200

    update page d'accueil + page entreprises
    Ajout des titres et modification de la page d'accueil. Ajout d'un calendrier google agenda sur la page entreprise

--- site/js/index.js
+++ site/js/index.js
@@ -1,8 +1,28 @@
-/* Même codee avec jquery */
 $(function(){
     AOS.init();
 
     let logo_congres = $('.main-image');
+    let titre_dernir_rubrique = $('#titre_derniere_rubrique');
+    let titre_historique = $('#historique');
+
+    if($(window).width() > 750){
+        titre_dernir_rubrique.html("L'organisation du congrès industriel <div class='underline'></div>");
+        titre_historique.html("L'historique du Congrès Industriel<div class='underline'></div>");
+    } else {
+        titre_dernir_rubrique.html("L'organisation du <div class='underline'></div>congrès industriel<div class='underline'></div>");
+        titre_historique.html("L'historique du<div class='underline'></div>Congrès Industriel<div class='underline'></div>");
+    }
+
+    function redimensionnement(){
+        if($(window).width() > 750){
+            titre_dernir_rubrique.html("L'organisation du congrès industriel <div class='underline'></div>");
+            titre_historique.html("L'historique du Congrès Industriel<div class='underline'></div>");
+        } else {
+            titre_dernir_rubrique.html("L'organisation du <div class='underline'></div>congrès industriel<div class='underline'></div>");
+            titre_historique.html("L'historique du<div class='underline'></div>Congrès Industriel<div class='underline'></div>");
+        }
+    }
+    $(window).on('resize',redimensionnement);
 
     logo_congres.fadeIn(1500);
 
